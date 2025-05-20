928610609041cf2880275a1a229314fe77217da0
ZhikangNiu <nzk020109@163.com>
2025-05-20 13:23:11 +0800

    first commit adma

---
+++ src/f5_tts/model/__init__.py
@@ -0,0 +1,10 @@
+from f5_tts.model.backbones.dit import DiT
+from f5_tts.model.backbones.dit_adma import DiT as DiT_ADMA
+from f5_tts.model.backbones.mmdit import MMDiT
+from f5_tts.model.backbones.unett import UNetT
+from f5_tts.model.cfm import CFM
+from f5_tts.model.cfm_adma import CFM as CFM_ADMA
+from f5_tts.model.trainer import Trainer
+
+
+__all__ = ["CFM", "CFM_ADMA", "UNetT", "DiT", "DiT_ADMA", "MMDiT", "Trainer"]
